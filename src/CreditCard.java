4a5a0c3cfb24b62cb9808115c0e90830ea5610d2
tchnxcal <tchnxcal@gmail.com>
2024-12-01 23:20:50 -0500

    Balance Check Method
    Checks balance via a method and prints error if payment exceeds balance amount
    + Corrected errors within code belonging

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -4,30 +4,39 @@ public class CreditCard {
     private Money creditLimit; // Creates a creditLimit object with the use of money object
     private Person owner; // Creates an owner object with the use of person object
 
-public CreditCard(Money creditLimit, Person owner) {
-    this.creditLimit = creditLimit;
-    this.owner = owner;
-    this.balance = new Money(0); // Creates balance based on money object with an initial value of 0
-    // Constructor method, automatically called when "new" is written to create an object
-}
+    public CreditCard(Person owner, Money creditLimit) {
+        this.creditLimit = creditLimit;
+        this.owner = owner;
+        this.balance = new Money(0); // Creates balance based on money object with an initial value of 0
+        // Constructor method, automatically called when "new" is written to create an object
+    }
 
-public Money getBalance() {
-    return new Money(this.balance); // New object ensures that the owner cannot manipulate the actual values inside CreditCard
-}
+    public Money getBalance() {
+        return new Money(this.balance); // New object ensures that the owner cannot manipulate the actual values inside CreditCard
+    }
 
-public Money getCreditLimit() {
-    return new Money(this.creditLimit);
-}
+    public Money getCreditLimit() {
+        return new Money(this.creditLimit);
+    }
 
-public String getOwnerInfo() {
-    return owner.toString();
-}
+    public String getPersonals() {
+        return owner.toString();
+    }
 
-public void charge(Money amount) {
-    Money newBalance = this.balance.add(amount);
-    if (newBalance.compareTo(this.creditLimit) > 0) {
-        System.out.println("Error: Charge exceeds credit limit. Transaction declined.");
-    } else {
-        this.balance = newBalance;
+    public void charge(Money amount) {
+        Money newBalance = this.balance.add(amount);
+        if (newBalance.compareTo(this.creditLimit) > 0) {
+            System.out.println("Error: Charge exceeds credit limit. Transaction declined.");
+        } else {
+            this.balance = newBalance;
+        }
+    }
 
+    public void payment(Money amount) {
+        if (amount.compareTo(this.balance) > 0) {
+            System.out.println("Error: Payment exceeds current balance.");
+        } else {
+            this.balance = this.balance.subtract(amount); // Subtract payment from balance
+        }
+    }
 }
